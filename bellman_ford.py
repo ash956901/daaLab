25c18c678f5001acd53e2dcb8fe7e95ad843ca52
Ashutosh Kumar <123724919+ash956901@users.noreply.github.com>
2024-07-05 18:08:09 +0530

    Update bellman_ford.py

--- bellman_ford.py
+++ bellman_ford.py
@@ -1,46 +1,37 @@
-#Bellman Ford
 class Graph:
-    def __init__(self,vertices):
-        self.vertices=vertices
-        self.edges=[]
-        self.visited=[False] * self.vertices
+    def __init__(self, vertices):
+        self.vertices = vertices
+        self.edges = []
+
+    def add_edge(self, u, v, w):
+        self.edges.append((u, v, w))
         
-    def add_edge(self,u,v,w):
-        self.edges.append((u,v,w))
+    def bellman_ford(self, src):
+        distances = [float('inf')] * self.vertices
+        distances[src] = 0
         
-    def bellman_ford(self,source):
-        distances=[float('inf')] * self.vertices
-        distances[source]=0
-        n=self.vertices
-        
-        #n-1 times
-        for i in range(n-1):
-            for u,v,w in self.edges:
-                if(distances[v]>distances[u]+w):
-                    distances[v]=distances[u]+w
-                    
-        #nth time
-        for u,v,w in self.edges:
-            if(distances[v]>distances[u]+w):
-                return -1
-            
+        for i in range(self.vertices - 1):
+            for u, v, w in self.edges:
+                if distances[u] != float('inf') and distances[u] + w < distances[v]:
+                    distances[v] = distances[u] + w
+
+
+        for u, v, w in self.edges:
+            if distances[u] != float('inf') and distances[u] + w < distances[v]:
+                return False 
+
         return distances
-    
-if __name__ =="__main__":
-    g = Graph(5)
-    g.add_edge(0, 1, -1)
-    g.add_edge(0, 2, 4)
-    g.add_edge(1, 2, 3)
-    g.add_edge(1, 3, 2)
-    g.add_edge(1, 4, 2)
-    g.add_edge(3, 2, 5)
-    g.add_edge(3, 1, 1)
-    g.add_edge(4, 3, -3)
-    
-    d=g.bellman_ford(0)
-    if d!=-1 :
-        print(d)
-    else:
-        print("Negative Cycle found")
-        
-        
+
+
+g = Graph(5)
+g.add_edge(0, 1, -1)
+g.add_edge(0, 2, 4)
+g.add_edge(1, 2, 3)
+g.add_edge(1, 3, 2)
+g.add_edge(1, 4, 2)
+g.add_edge(3, 2, 5)
+g.add_edge(3, 1, 1)
+g.add_edge(4, 3, -3)
+
+result = g.bellman_ford(0)
+print(result)  
